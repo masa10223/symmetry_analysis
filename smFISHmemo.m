8f2c65d5863fc7efca1c5ecb212df2349595e225
ichii-shunsuke <84959143+ichii-shunsuke@users.noreply.github.com>
2021-10-21 18:11:00 +0900

    highlightFP

--- smFISHmemo.m
+++ smFISHmemo.m
@@ -56,9 +56,51 @@ J3 = imsubtract(imadd(I3,imtophat(I3,se3)),imbothat(I3,se3));
 figure
 imshow(J3)
 
+% パラメータを調整
+close all
+I = squeeze(data(1,1,1,:,:));
+se = strel('disk',6);
+J = imsubtract(imadd(I,imtophat(I,se)),imbothat(I,se));
+figure
+imshow(J)
+I2 = imgaussfilt(J,1);
+se2 = strel('disk',3);
+J2 = imsubtract(imadd(I2,imtophat(I2,se2)),imbothat(I2,se2));
+figure
+imshow(J2)
+I3 = imgaussfilt(J2,1);
+se3 = strel("disk", 1);
+J3 = imsubtract(imadd(I3,imtophat(I3,se3)),imbothat(I3,se3));
+figure
+imshow(J3)
+
 % J3のうち上位0.5%の輝度の部分を抽出
 top = zeros(size(J3));
 [~,ind] = maxk(J3(:),ceil(size(J3(:),1)*0.005));
 top(ind) = 1;
 figure
 imshow(top);
+
+% topのうちareaが小さいやつを除く
+L = bwlabel(top);
+stats = regionprops(L, "Area");
+for i = 1:size(stats, 1)
+    if stats(i).Area < 3
+        L(L==i) = 0;
+    end
+end
+imshow(top.*(L>0));
+
+% 検出されたものをtmrに重ねて見る
+colorFig = repmat(squeeze(data(1,1,1,:,:)), [1,1,3]);
+existence = zeros(size(colorFig), "logical");
+existence(:,:,1) = L>0;
+colorFig(existence) = 512;
+imshow(colorFig)
+
+
+
+RawData = lsmread("RawData/smFISH/20211011/NoProbe/noprobe_211011 _sample1_1_DAPI%=2_TMR%=10_sum=4_size=512_interval=0_slice=1.lsm");
+RawData = lsmread("RawData/smFISH/20211011/probe conc. 2%/ndr2_211011 _sample1_2_DAPI%=1_TMR%=10_TMRgain=900__sum=4_size=512_interval=0_slice=1.lsm");
+tmr = rescale(double(squeeze(RawData(1,1,1,:,:))));
+dapi = rescale(double(squeeze(RawData(1,2,1,:,:))));
